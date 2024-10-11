a278a37c11437820b993d55da6c334fe9885c02f
github-mester21321 <11c-sveiczer@ipari.vein.hu>
2024-10-11 17:04:10 +0000

    asdasd

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-#kiegeszitok beimportalasa
-from tkinter import*
-
-root = Tk()
-root.geometry("400x600")
-root.configure(bg='#6a6664')
-
-felhasznalonev = Entry(root).place(relx=0.25, y=20, anchor=CENTER)
-jelszo = Entry(root).place(relx=0.75, y=20, anchor=CENTER)
-
-
-root.mainloop()
+print("asd")
